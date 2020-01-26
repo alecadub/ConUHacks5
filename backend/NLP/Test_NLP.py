08bafc75feaab555e8802f5b6b478e5ac62cf56a
kiki <kaylasharky@hotmail.com>
2020-01-25 23:57:50 -0500

    fixed master and added to requirements

--- backend/NLP/Test_NLP.py
+++ backend/NLP/Test_NLP.py
@@ -1,14 +1,14 @@
-import sentiment_analysis as sentiment_analysis
-import T2S_test as speech2text
+# import sentiment_analysis as sentiment_analysis
+# import T2S_test as speech2text
 
-def getSentimentOfText(audio_file):
-    # Get text from audio
-    text = speech2text.sample_long_running_recognize(audio_file)
-    print('Text from Speech: ', text)
-    # Get Sentiment of text 
-    text_sentiment = sentiment_analysis.analyze(text)
-    print('Sentimant Found', text_sentiment)
-    print('Result => Text: {} Sentiment : {}'.format(text, text_sentiment))
+# def getSentimentOfText(audio_file):
+#     # Get text from audio
+#     text = speech2text.sample_long_running_recognize(audio_file)
+#     print('Text from Speech: ', text)
+#     # Get Sentiment of text 
+#     text_sentiment = sentiment_analysis.analyze(text)
+#     print('Sentimant Found', text_sentiment)
+#     print('Result => Text: {} Sentiment : {}'.format(text, text_sentiment))
 
 
-getSentimentOfText('resources/brooklyn_bridge.raw')
+# getSentimentOfText('resources/brooklyn_bridge.raw')
