d362c3d5c70b763d215d04d85ba86f6b35b3f1bf
Alec Adub <adub.alec@gmail.com>
2020-01-26 01:22:08 -0500

    Speech to text

--- frontend/harmlessBuddy/src/app/tab2/tab2.page.ts
+++ frontend/harmlessBuddy/src/app/tab2/tab2.page.ts
@@ -1,15 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { ApiService } from '../services/api';
-
+declare var webkitSpeechRecognition: any;
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
-  moodyMessages: any;
-
+  @ViewChild('gSearch', { static: false }) formSearch;
+  @ViewChild('searchKey', { static: false }) hiddenSearchHandler;
   constructor(public api: ApiService) {}
 
   ngOnInit() {}
+
+  public voiceSearch() {
+    if ('webkitSpeechRecognition' in window) {
+      const vSearch = new webkitSpeechRecognition();
+      console.log(vSearch);
+      vSearch.continuous = false;
+      vSearch.interimresults = false;
+      vSearch.lang = 'en-US';
+      vSearch.start();
+      const voiceHandler = this.hiddenSearchHandler.nativeElement;
+      vSearch.onresult = function(data) {
+        voiceHandler.value = data.results[0][0].transcript;
+        console.log(data.results[0][0].transcript);
+        vSearch.stop();
+      };
+      vSearch.onerror = function(e) {
+        console.log(e);
+      };
+    }
+  }
 }
